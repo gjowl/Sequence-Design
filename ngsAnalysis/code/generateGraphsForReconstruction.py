394af9dfd2b9ab0182c44d14e9e2253b02f115f5
Gilbert-linux <loiseau@wisc.edu>
2022-06-01 13:51:48 -0500

    outputs graph with diff colors; next trendline

--- ngsAnalysis/code/generateGraphsForReconstruction.py
+++ ngsAnalysis/code/generateGraphsForReconstruction.py
@@ -1,5 +1,10 @@
 import sys
 from functions import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as cs
+import random
 
 """
 Run as:
@@ -15,9 +20,45 @@ I will use this to input datafiles generate by other pieces of code that can the
 analyzed.
 """
 
+def createScatterPlot(x, y, labels, color, outFile, title):
+    fig, ax = plt.subplots()
+    #obtain m (slope) and b(intercept) of linear regression line
+    m, b = np.polyfit(x, y, 1)
+    #add linear regression line to scatterplot 
+    plt.plot(x, m*x+b, label='y={:.2f}x+{:.2f}'.format(m, b))
+    
+    # set axis title
+    ax.set_title(title)
+
+    # set axes labels
+    ax.set_xlabel('Energy Score')
+    ax.set_ylabel('Fluorescence')
+    # get a color map based on the length of the input
+    colormap = cm.viridis
+    colorlist = [cs.rgb2hex(colormap(i)) for i in np.linspace(0, 0.9, len(labels))]
+    for i,c in enumerate(colorlist):
+        x1=x[i]
+        y1=y[i]
+        l=labels[i]
+        ax.scatter(x1, y1, label=l, s=50, linewidth=0.1, c=c)
+    
+    #for n in range(len(labels)):
+    #    plt.scatter(xAxis, yAxis, label=labels[n])
+    #l = plt.legend(labels, loc=len(labels)) 
+
+    
+    l1 = plt.legend(fontsize=9)
+    l2 = plt.legend(fontsize=6, loc='center right')
+    #plt.gca().add_artist(l)
+    #plt.gca().add_artist(l1)
+    #plt.gca().add_artist(l2)
+    # save image to filename
+    fig.savefig(outFile,format='png', dpi=1200)
+
 #MAIN
 # variables: if want to make this more multipurpose, add the below into a config file
-outputDir = '/mnt/c/Users/gjowl/github/Sequence-Design/ngsAnalysis/2022-5-13/graphs/'
+#outputDir = '/mnt/c/Users/gjowl/github/Sequence-Design/ngsAnalysis/2022-5-13/graphs/'
+outputDir = '/exports/home/gloiseau/github/Sequence-Design/ngsAnalysis/2022-5-13/graphs/'
 columnsToAnalyze = ['Total']
 
 # make the output directory if it doesn't exist
@@ -30,15 +71,32 @@ df = pd.read_csv(inputFile)
 # get a list of dataframes of all sequences that are from the same design group (runNumber or StartSequence)
 list_designDf = []
 if 'runNumber' in df.columns:
-    for num in df['runNumber']:
+    for num in df['runNumber'].unique():
         designDf = pd.DataFrame()
         designDf = df[df['runNumber'] == num]
-        list_designDf = designDf
+        list_designDf.append(designDf)
 else:
     for sequence in df['StartSequence']:
         designDf = pd.DataFrame()
         designDf = df[df['StartSequence'] == sequence]
-        list_designDf = designDf
+        list_designDf.append(designDf)
+
+for designDf in list_designDf:
+    designDf = designDf[designDf['Total'] < 100]
+    # sorts the designDf by Total energy score
+    designDf = designDf.sort_values(by='Total')
+    xAxis = designDf['Total']
+    yAxis = designDf['Average'] # Fluorescence
+    runNumber = designDf['runNumber'][2]
+    graphTitle = 'Design '+str(runNumber)
+    graphFile = outputDir+graphTitle+'.png'
+    label = designDf['Sequence']
+    colors = []
+    for j in range(len(xAxis)):
+        colors.append("#"+''.join([random.choice('ABCDEF0123456789') for i in range(6)]))
+    createScatterPlot(xAxis,yAxis,label,colors,graphFile,graphTitle)
+    exit()
+
 
 # TODO: add in a way to identify any sequences with the same positions for the interface
 # I think take the interface, identify which positions are not dash, add that number to a list
@@ -64,7 +122,7 @@ for interface in df['Interface']:
     for numInterface in list_interface:
         for index in numInterface:
             if interface[index] == '-':
-                
+               continue 
 
 # TODO: input data here for kde plotting; go through the xShifts and crossingAngles and such here for any
 # generated dataframes
