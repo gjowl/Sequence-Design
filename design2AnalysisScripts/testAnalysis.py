5a7b1cd70b2c27177a7e0a7557ecb52926c3a2f4
gjowl <gjowl04@gmail.com>
2022-09-05 21:59:52 -0500

    started on test analysis

--- design2AnalysisScripts/testAnalysis.py
+++ design2AnalysisScripts/testAnalysis.py
@@ -1,22 +1,28 @@
 import sys
 import pandas as pd
 import os
+from plotGeomKde import *
 
 # read in data file
 datafile = sys.argv[1]
+kdeFile = os.getcwd()+'/' + '2020_09_23_kdeData.csv'
 
 # read in the data
-df = pd.read_csv(datafile, sep='\t', header=0)
+df_data = pd.read_csv(datafile, sep=',', header=0)
+df_kde = pd.read_csv(kdeFile)
 
 # trim data
 # only keep sequences where Total Energy is less than 0
-df = df[df['Total'] < 0]
+print(df_data)
+df_total = df_data[df_data['Total'] < 0]
 
 # only keep sequences where VDWDiff is greater than 0
-df = df[df['VDWDiff'] > 0]
+df_vdwDiff = df_data[df_data['VDWDiff'] > 0]
 
+df_list = [df_data]
 # TODO: setup overlay for a list of dataframes (also get the overlays for axial and z)
-
+for df in df_list:
+    plotGeomKde(df_kde, df)
 # TODO: only overlay unique geometries
 # TODO: compare the geometries to the sequences I got before; to the sequences that worked plot
 
