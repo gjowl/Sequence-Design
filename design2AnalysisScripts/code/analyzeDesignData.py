3b2baa9f4b27aac3fd5a2f2dd5c2dd685ef0fd19
gjowl <gjowl04@gmail.com>
2022-09-12 19:04:29 -0500

    added in ideas for analysis

--- design2AnalysisScripts/code/analyzeDesignData.py
+++ design2AnalysisScripts/code/analyzeDesignData.py
@@ -13,26 +13,49 @@ import os
 # Read in the data from the csv file
 df = pd.read_csv(sys.argv[1])
 
+# only keep sequences that where total < 0
+df = df[df['Total'] < 0]
+
+# get the current working directory
+cwd = os.getcwd()+"/"
+
 # get a dataframe with sequences that are not unique
 dfDup = df[df.duplicated(subset=['Sequence'], keep=False)]
+# sort the dataframe by sequence and total
+dfDup = dfDup.sort_values(by=['Sequence', 'Total'])
+dfDup.to_csv(cwd+"duplicateSequences.csv", index=False)
 
 # get a dataframe with sequences that are unique
 dfUnique = df.drop_duplicates(subset=['Sequence'], keep='first')
+
 # sort by total
 dfUnique = dfUnique.sort_values(by=['Total'])
-print(dfUnique)
-exit()
 
-# separate the df into regions of interest
-dfRight = df[df['crossingAngle'] < 0]
-dfgxxxg = dfRight[dfRight['crossingAngle'] < 0]
-dfLeft = df[df['crossingAngle'] > 0]
+# output dfUnique to a csv file
+dfUnique.to_csv(cwd+"uniqueSequences.csv", index=False)
 
-# get the average energy for each region of interest
-avgEnergyRight = dfRight['Energy'].mean()
-avgEnergyGxxxg = dfgxxxg['Energy'].mean()
-avgEnergyLeft = dfLeft['Energy'].mean()
+## separate the df into regions of interest
+#dfRight = df[df['crossingAngle'] < 0]
+#dfgxxxg = dfRight[dfRight['crossingAngle'] < 0]
+#dfLeft = df[df['crossingAngle'] > 0]
+#
+## get the average energy for each region of interest
+#avgEnergyRight = dfRight['Energy'].mean()
+#avgEnergyGxxxg = dfgxxxg['Energy'].mean()
+#avgEnergyLeft = dfLeft['Energy'].mean()
  
 # what plots should I use? scatter plot for each?
 
+
+
 # ideas for analysis
+"""
+    - scatterplots for each
+    - histograms for top x (100?) designs; compare to previous designs in CHIP1
+    - compare geometries from duplicate sequences
+    - compare geometries from non-duplicate sequences (kde plots for this and above?)\
+    - look at more structures
+    - plots for vdw and other energy differences
+    - look at the energy differences between the top 100 designs and the rest of the designs
+    - scatterplots of predicted fluorescence...?
+"""
