9f654bb99970e61fb56ce0b74f871186f8f09aad
Gilbert-linux <loiseau@wisc.edu>
2023-02-14 13:14:10 -0600

    slight changes to scatter outputs

--- 2022_designAnalysisScripts/CHIP1_reanalysis/calcEnergy/code/analyzeData.py
+++ 2022_designAnalysisScripts/CHIP1_reanalysis/calcEnergy/code/analyzeData.py
@@ -17,19 +17,44 @@ df = df.drop_duplicates(subset=['Sequence'], keep='first')
 df = df[df['PercentGpaStdDev'] < 10]
 
 # rid of any sequences where the PercentGpa < 40 (G83I)
-df = df[df['PercentGpa'] > 0]
+df_G83I = df[df['PercentGpa'] > 40]
 
-# make a scatter plot of the total energy vs the percent gpa
-df.plot.scatter(x='Total', y='PercentGpa', title='Total Energy vs Percent GpA')
+# rid of any sequences where the PercentGpa < 50 (G83I+10, somewhat arbitrary in terms of dimerization or not)
+df_G83I_dimer = df[df['PercentGpa'] > 45]
 
-# add in the standard deviation
-plt.errorbar(df['Total'], df['PercentGpa'], yerr=df['PercentGpaStdDev'], fmt='none', ecolor='black')
+# add all dfs to a list
+dfs = [df, df_G83I, df_G83I_dimer]
 
-# add a line of best fit and an r^2 value
-m, b = np.polyfit(df['Total'], df['PercentGpa'], 1)
-plt.plot(df['Total'], m*df['Total'] + b, color='red')
+# make a list of names for the dfs
+dfsNames = ['All', 'G83I', 'LikelyDimer']
 
-# add the r^2 value to the top left of the plot
-r2 = np.corrcoef(df['Total'], df['PercentGpa'])[0,1]**2
-plt.text(0.05, 0.95, f'r^2 = {r2:.2f}', transform=plt.gca().transAxes, fontsize=14, verticalalignment='top')
-plt.savefig(f'{outputDir}/totalEnergyVsPercentGpA.png')
+# loop through the dfs and make plots
+for df, name in zip(dfs, dfsNames):
+    # define the output directory
+    outDir = f'{outputDir}/{name}'
+
+    # make an output directory for the plots
+    os.makedirs(outDir, exist_ok=True)
+
+    # make a scatter plot of the total energy vs the percent gpa
+    df.plot.scatter(x='Total', y='PercentGpa', title='Total Energy vs Percent GpA')
+
+    # set the y axis to be between 0 and a tens place near the max
+    plt.ylim(0, int(df['PercentGpa'].max()/10)*10+20)
+
+    # add in the standard deviation
+    plt.errorbar(df['Total'], df['PercentGpa'], yerr=df['PercentGpaStdDev'], fmt='none', ecolor='black')
+    plt.text(0.99, 1.10, f'N = {len(df)}', transform=plt.gca().transAxes, fontsize=14, verticalalignment='top', horizontalalignment='right')
+    plt.savefig(f'{outDir}/scatter_EnergyVsPercentGpA.png')
+
+    # add a line of best fit and an r^2 value
+    m, b = np.polyfit(df['Total'], df['PercentGpa'], 1)
+    plt.plot(df['Total'], m*df['Total'] + b, color='red')
+
+    # add the r^2 value to the top left above the plot
+    r2 = np.corrcoef(df['Total'], df['PercentGpa'])[0,1]**2
+    plt.text(0.01, 1.10, f'r^2 = {r2:.2f}', transform=plt.gca().transAxes, fontsize=14, verticalalignment='top')
+    plt.savefig(f'{outDir}/scatter_regression.png')
+
+    # output the df to a csv file
+    df.to_csv(f'{outDir}/totalEnergyVsPercentGpA.csv')
