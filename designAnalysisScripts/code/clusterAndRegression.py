66a0381d0d932906b24cc6de5272747959125b73
gjowl <gjowl04@gmail.com>
2023-03-10 21:35:44 -0600

    - extra imports

--- designAnalysisScripts/code/clusterAndRegression.py
+++ designAnalysisScripts/code/clusterAndRegression.py
@@ -1,7 +1,6 @@
 import os, sys, random, pandas as pd
 import numpy as np, seaborn as sns
 import matplotlib.pyplot as plt
-from kneed import KneeLocator
 from sklearn import preprocessing, svm
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression as lr
