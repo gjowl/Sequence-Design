4513563d128cc2c258b80a32b9421c8644ee4edc
gilbert.work <loiseau@wisc.edu>
2023-03-13 15:22:52 -0500

    added in evaluation of cluster number

--- designAnalysisScripts/code/clusterPipeline.py
+++ designAnalysisScripts/code/clusterPipeline.py
@@ -13,21 +13,63 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 This pipeline takes in a dataframe, preproccesses it, and clusters it using k-means clustering.
 """
 
+# the below analyzes the data to get the best number of components to cluster with
+def getBestClusterNumber(pipe, cluster_data, true_labels, max_components):
+    # Empty lists to hold evaluation metrics
+    silhouette_scores, ari_scores = [], []
+    for n in range(2, max_components):
+        # This set the number of components for pca
+        pipe["preprocessor"]["pca"].n_components = n
+        pipe.fit(cluster_data)
+   
+        # calculate the silhouette score and ari score
+        silhouette_coef = silhouette_score(
+            pipe["preprocessor"].transform(cluster_data),
+            pipe["clusterer"]["kmeans"].labels_,
+        )
+        ari = adjusted_rand_score(
+            true_labels,
+            pipe["clusterer"]["kmeans"].labels_,
+        )
+   
+        # Add metrics to their lists
+        silhouette_scores.append(silhouette_coef)
+        ari_scores.append(ari)
+
+    # get the best number of components by finding the max of the ari scores
+    n_components = np.argmax(ari_scores) + 2
+    print(f'Best number of components: {n_components}')
+
+    # Plot the evaluation metrics
+    plt.figure(figsize=(8, 8))
+    plt.plot(range(2, max_components), silhouette_scores, c="#008fd5", label="Silhouette Coefficient")
+    plt.plot(range(2, max_components), ari_scores, c="#fc4f30", label="ARI")
+   
+    plt.xlabel("n_components")
+    plt.legend()
+    plt.title(f'Clustering Performance as a Function of {max_components} components')
+    plt.tight_layout()
+
+    # save the plot
+    plt.savefig(f'{output_dir}/getBestCluster.png')
+    plt.clf()
+    return n_components
+
 if __name__ == "__main__":
     # read in a dataframe from the command line
     df = pd.read_csv(sys.argv[1], dtype={'Interface': str})
     output_dir = sys.argv[2]
 
+    # make the output directory if it doesn't exist
+    os.makedirs(name=output_dir, exist_ok=True)
+
     # keep only data where total < -5
     df = df[df['Total'] < -5]
 
     # columns of data to be used for clustering
-    #cols = ['endXShift', 'endCrossingAngle', 'endAxialRotation', 'endZShift', 'Total']
-    cols = ['endXShift', 'endCrossingAngle', 'Total']
-    cluster_cols = ['endXShift', 'endCrossingAngle', 'endAxialRotation', 'endZShift', 'VDWDiff', 'HBONDDiff', 'IMM1Diff', 'Total']
+    cols = ['endXShift', 'endCrossingAngle', 'endAxialRotation', 'endZShift', 'VDWDiff', 'HBONDDiff', 'IMM1Diff', 'Total']
     n_clusters = 3
 
-# TODO: figure out how to decrease the dimensions of the data before clustering; how many components and which ones
     # implement PCA to reduce the number of dimensions
     preproccessor = Pipeline([
         ('scaler', MinMaxScaler()),
@@ -53,6 +95,16 @@ if __name__ == "__main__":
 
     # fit the pipeline to the data (executes the preproccessor and clusterer on the data)
     cluster_data = df[cols]
+    
+    # get the true labels (in this case with all the data, the region)
+    label_encoder = LabelEncoder()
+    true_labels = label_encoder.fit_transform(df['Region'].values)
+
+    # get the best number of components to cluster with
+    n_components = getBestClusterNumber(pipe, cluster_data, true_labels, len(cols))
+
+    # fit the pipeline to the data (executes the preproccessor and clusterer on the data)
+    pipe["preprocessor"]["pca"].n_components = n_components
     pipe.fit(cluster_data)
 
     preproccessed_data = pipe['preprocessor'].transform(cluster_data)
@@ -63,19 +115,19 @@ if __name__ == "__main__":
     # output the silhouette score
     print(f'Silhouette Score: {silhouette_score(preproccessed_data, cluster_labels)}')
 
-    # get the true labels (in this case with all the data, the region)
-    label_encoder = LabelEncoder()
-    true_labels = label_encoder.fit_transform(df['Region'].values)
-
     # output the adjusted rand score
     print(f'Adjusted Rand Score: {adjusted_rand_score(true_labels, cluster_labels)}')
 
+    # make the pca columns list
+    pca_cols = []
+    for i in range(n_components):
+        pca_cols.append(f'component_{i+1}')
+    
     # plot the data with the cluster labels
     pcadf = pd.DataFrame(
         pipe["preprocessor"].transform(cluster_data),
-        columns=["component_1", "component_2"],
+        columns=pca_cols
     )
-    
     pcadf["predicted_cluster"] = pipe["clusterer"]["kmeans"].labels_
     pcadf["true_label"] = label_encoder.inverse_transform(true_labels)
 
