a52b11f21f759bae3e878337b8406bb999300e3c
gilbert.work <loiseau@wisc.edu>
2023-04-18 17:42:59 -0500

    smarter way to output mutant values

--- CHIP_design_scripts/code/checkForGAS.py
+++ CHIP_design_scripts/code/checkForGAS.py
@@ -8,8 +8,7 @@ positions = [int (i) for i in sys.argv[3:]]
 # read the input file as a dataframe
 df = pd.read_csv(input_file, sep=',')
 
-aa = ['G']
-#aa = ['G', 'A', 'S']
+aa = ['G', 'A', 'S']
 for pos in positions:
     # get the aa at the positions in the sequence
     df[f'AA{pos}'] = df['Sequence'].str[pos-1]
@@ -23,11 +22,23 @@ if len(positions) > 1:
     df = df[df['AAs'].str.count('|'.join(aa)) >= 2]
 else:
     df = df[df['AAs'].str.count('|'.join(aa)) >= 1]
-print(df)
 
-# remove the columns containing aa
-df = df.drop([f'AA{pos}' for pos in positions], axis=1)
-df = df.drop(['AAs'], axis=1)
+# get the number of rows that have G in each position
+total = df.shape[0]
+print(f'Total number of sequences: {total}')
+for pos in positions:
+    print(f'{pos}:')
+    print(f'Number of sequences with GAS in position {pos}: {df[df[f"AA{pos}"].isin(aa)].shape[0]}')
+    print(f'Number of sequences without GAS in position {pos}: {df[df[f"AA{pos}"] != "G"].shape[0]}')
+    # get the number of rows that do not have G in each position
+    pos_df = df[df[f'AA{pos}'].isin(aa)]
+    for pos2 in positions:
+        if pos != pos2:
+            print(f'Number of sequences with GAS in position {pos} and without G in position {pos2}: {pos_df[pos_df[f"AA{pos2}"] != "G"].shape[0]}')
+
+for pos in positions:
+    # get the number of rows that do not have GAS in each position
+    print(f'Number of sequences with GAS in position {pos}: {total-df[~df[f"AA{pos}"].isin(aa)].shape[0]}')
 
 # output the dataframe to a csv file without the index
 df.to_csv(f'{output_file}_{positions}.csv', index=False)
