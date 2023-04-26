aa036c283a24ad1a02312a948cc47d441d25c50f
gilbert.work <loiseau@wisc.edu>
2023-04-26 15:46:54 -0500

    works given cutoff_col for up to 5 color/cutoffs

--- figureMaking/code/interfacePdbColorByValues.py
+++ figureMaking/code/interfacePdbColorByValues.py
@@ -50,25 +50,40 @@ if __name__ == '__main__':
     raw_data_dir = sys.argv[1]
     data_file = sys.argv[2]
     output_dir = sys.argv[3]
+    cutoff_column = sys.argv[4]
 
     # make the output directory if it doesn't exist
     os.makedirs(name=output_dir, exist_ok=True)
-    png_dir, pse_dir = output_dir+'/png', output_dir+'/pse'
+    png_dir, pse_dir = f'{output_dir}/{cutoff_column}/png', f'{output_dir}/{cutoff_column}/pse'
 
     # make the png and pse directories if they don't exist
-    os.makedirs(name=output_dir+'/png', exist_ok=True)
-    os.makedirs(name=output_dir+'/pse', exist_ok=True)
+    os.makedirs(name=png_dir, exist_ok=True)
+    os.makedirs(name=pse_dir, exist_ok=True)
 
     # read in the data file
     df = pd.read_csv(data_file, sep=',', dtype={'Interface': str})
 
+    # define the possible color values in increasing order of intensity
+    color_values = ['salmon', 'deepsalmon', 'red', 'firebrick', 'ruby']
+    color_dict = {}
+    # loop through the cutoff values
+    cutoff_values = df[cutoff_column].unique()
+    for i in range(0, len(cutoff_values)):
+        # get the cutoff value
+        cutoff = cutoff_values[i]
+        # add the cutoff value to the dictionary
+        color_dict[cutoff] = color_values[i]
+
     # loop through the entire dataframe
-    for i in range(len(df)):
+    for sequence in df['Sequence'].unique():
+        # get the dataframe for the current sequence
+        df_seq = df[df['Sequence'] == sequence]
         # get the directory name
-        dirName = df['Directory'][i]
+        dirName = df_seq['Directory'].values[0]
+        print(df_seq)
         # get the design number by splitting the directory name by _
-        designNum, repNum = dirName.split('_')[1], df['replicateNumber'][i]
-        # pdbName
+        designNum, repNum = dirName.split('_')[1], df_seq['replicateNumber'].values[0]
+        # pdbName (all should have same pdb name)
         pdbName = designNum+'_'+str(repNum)
         # put together the filename
         filename = raw_data_dir+'/'+dirName+'/'+pdbName+'.pdb'
@@ -77,10 +92,16 @@ if __name__ == '__main__':
         # setup the pymol session for output
         setupPymol()
         # get the interface positions as a residue string
-        selection_string = getInterfacePos(df['Interface'][i])
+        selection_string = getInterfacePos(df_seq['Interface'].values[0])
         cmd.select('interface', selection_string)
-        # color the residue for the current pdb on chain A
-        cmd.color('red', 'interface and chain A')
+        # loop through the positions in the dataframe
+        for position in df_seq['Position']:
+            # get the cutoff value for the current position
+            cutoff = df_seq[df_seq['Position'] == position][cutoff_column].values[0]
+            # get the color for the current position
+            color = color_dict[cutoff]
+            # color the residue for the current pdb on chain A
+            cmd.color(color, f'interface and resi {position+23} and chain A')
         # save the session file and png file
         cmd.save(pse_dir+'/'+pdbName+'.pse')
         cmd.png(png_dir+'/'+pdbName+'.png', width=1000, height=1000, ray=3, dpi=300)
