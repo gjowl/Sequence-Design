29c0539873e158a8db2333cbe40419e95898667d
gilbert.work <loiseau@wisc.edu>
2023-09-17 16:24:09 -0500

    code works, but fit is quite poor; what next?

--- CHIP2/regressionTraining/simpleLinearRegression.py
+++ CHIP2/regressionTraining/simpleLinearRegression.py
@@ -1,16 +1,27 @@
-import os, sys, numpy as np, pandas as pd
+import os, sys, numpy as np, pandas as pd, matplotlib.pyplot as plt
+from sklearn import preprocessing, svm
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error,mean_squared_error
+ 
 
 # read in command line arguments
 dataFile = sys.argv[1]
 outputDir = sys.argv[2]
 
+os.makedirs(name=outputDir, exist_ok=True)
 # read in the data file
 df = pd.read_csv(dataFile)
 
 # hardcoded: choose which columns to use for the regression
 xAxis = 'PercentGpA'
 yAxis = 'Total'
+
+# remove any rows where sample is NA
+df = df[df['Sample'].notna()]
+df = df[df[yAxis] < -5]
+df = df[df[xAxis] < 2]
+df = df[df[xAxis] > 0.35]
 
 # loop through the design types
 for design in df['Sample'].unique():
@@ -22,7 +33,7 @@ for design in df['Sample'].unique():
     model = LinearRegression().fit(x, y)
 
     # the below I got from: https://www.geeksforgeeks.org/python-linear-regression-using-sklearn/#
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.75, random_state=0)
 
     regr = LinearRegression()
 
@@ -35,5 +46,14 @@ for design in df['Sample'].unique():
 
     # save the plot
     plt.savefig(f'{outputDir}/{design}.png')
+    plt.clf()
     # TODO: I also have a script from a year ago in clusterAnalysis/code/linearRegression.py; could look at that for more detail and can't remember if it worked?
     # that same directory also has some clustering code, and now with actual experimental data, could be used to cluster for meaningful data?
+
+    #squared True returns MSE value, False returns RMSE value.
+    mae = mean_absolute_error(y_true=y_test,y_pred=y_pred)
+    mse = mean_squared_error(y_true=y_test,y_pred=y_pred) #default=True
+    rmse = mean_squared_error(y_true=y_test,y_pred=y_pred,squared=False)
+    print("MAE:",mae)
+    print("MSE:",mse)
+    print("RMSE:",rmse)
