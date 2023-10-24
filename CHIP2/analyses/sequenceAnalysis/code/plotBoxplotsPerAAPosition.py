77f18ae6656976d8a0057a56b44f443bc50200b6
gjowl <gjowl04@gmail.com>
2023-10-23 19:41:31 -0500

    how to tell diffs between seqs w/ similar fluor

--- CHIP2/analyses/sequenceAnalysis/code/plotBoxplotsPerAAPosition.py
+++ CHIP2/analyses/sequenceAnalysis/code/plotBoxplotsPerAAPosition.py
@@ -32,8 +32,8 @@ xaxis = 'Sample'
 # plot the boxplots
 wt_filename = os.path.basename(sequenceFile).split('.')[0]
 mut_filename = os.path.basename(mutantFile).split('.')[0]
-#plotBoxplot(df_wt, xaxis, yaxis, outputDir, wt_filename)
-#plotBoxplot(df_mut, xaxis, yaxis, outputDir, mut_filename)
+plotBoxplot(df_wt, xaxis, yaxis, outputDir, wt_filename)
+plotBoxplot(df_mut, xaxis, yaxis, outputDir, mut_filename)
 
 df_mut['pos_wtAA'] = df_mut['Position'].astype(str) + df_mut['WT_AA']
 # get the mutant aa for each sequence, based on the position column
@@ -44,20 +44,30 @@ for sample in df_mut['Sample'].unique():
     df_sample = df_mut[df_mut['Sample'] == sample]
     sample_outputDir = f'{outputDir}/{sample}'
     os.makedirs(sample_outputDir, exist_ok=True)
-    #plotBoxplot(df_sample, 'Position', yaxis, sample_outputDir, mut_filename)
-    ## combine the position and mutant AA
-    #tmp_df = df_sample.groupby('mut_AA').filter(lambda x: len(x) > 10).copy()
-    #plotBoxplot(tmp_df, 'mut_AA', yaxis, sample_outputDir, mut_filename)
-    ## only keep rows that have > 10 posMutAA
-    #tmp_df = df_sample.groupby('pos_mutAA').filter(lambda x: len(x) > 10).copy()
-    #plotBoxplot(tmp_df, 'pos_mutAA', yaxis, sample_outputDir, mut_filename)
+    plotBoxplot(df_sample, 'Position', yaxis, sample_outputDir, mut_filename)
+    # combine the position and mutant AA
+    tmp_df = df_sample.groupby('mut_AA').filter(lambda x: len(x) > 10).copy()
+    plotBoxplot(tmp_df, 'mut_AA', yaxis, sample_outputDir, mut_filename)
+    # only keep rows that have > 10 posMutAA
+    tmp_df = df_sample.groupby('pos_mutAA').filter(lambda x: len(x) > 10).copy()
+    plotBoxplot(tmp_df, 'pos_mutAA', yaxis, sample_outputDir, mut_filename)
     # get the sequences from the df_wt that are present in the df_sample
     df_wt_sample = df_wt[df_wt['Sequence'].isin(df_sample['Sequence'])]
     tmp_df = df_sample[df_sample['Sequence'].isin(df_wt_sample['Sequence'])].copy()
     # make a column for wt_percentGpA that gets the vale from the df_wt_sample for matching sequences
     tmp_df['wt_percentGpA'] = tmp_df.apply(lambda row: df_wt_sample[df_wt_sample['Sequence'] == row['Sequence']]['PercentGpA_transformed'].values[0], axis=1)
     # keep only unique sequences
-    plotBoxplot(tmp_df, 'pos_wtAA', 'wt_percentGpA', sample_outputDir, mut_filename)
+    tmp_df1 = tmp_df.groupby('pos_mutAA').filter(lambda x: len(x) > 10).copy()
+    plotBoxplot(tmp_df1, 'pos_mutAA', 'wt_percentGpA', sample_outputDir, mut_filename)
+    tmp_df1 = tmp_df.groupby('pos_wtAA').filter(lambda x: len(x) > 10).copy()
+    plotBoxplot(tmp_df1, 'pos_wtAA', 'wt_percentGpA', sample_outputDir, mut_filename)
+    plotBoxplot(tmp_df, 'Position', 'wt_percentGpA', sample_outputDir, mut_filename)
+    #for position in tmp_df['Position'].unique():
+    #    tmp_df_pos = tmp_df[tmp_df['Position'] == position]
+    #    tmp_df_pos = tmp_df_pos.groupby('pos_mutAA').filter(lambda x: len(x) > 10).copy()
+    #    posFilename = f'pos{position}'
+    #    plotBoxplot(tmp_df_pos, 'pos_mutAA', 'wt_percentGpA', sample_outputDir, posFilename)
+    #    plotBoxplot(tmp_df_pos, 'pos_wtAA', 'wt_percentGpA', sample_outputDir, posFilename)
     
 # TODO: look at the wt data as well by getting the 
 #for sample in df_wt['Sample'].unique():
