b6cd4e9f64ccebefff8c891fb9fc26ec3150ad8b
gjowl <gjowl04@gmail.com>
2024-01-22 19:51:24 -0600

    edits for draft v2

--- CHIP2/analyses/figureMaking/code/compareTmDock.py
+++ CHIP2/analyses/figureMaking/code/compareTmDock.py
@@ -10,6 +10,7 @@ outputDir = sys.argv[3]
 os.makedirs(name=outputDir, exist_ok=True)
 
 # loop through the tmDock directory
+df = pd.DataFrame(columns=['Name', 'RMSD'])
 for file in os.listdir(tmDockDir):
     # get the pdb name
     pdbName = file.split('.')[0]
@@ -35,8 +36,8 @@ for file in os.listdir(tmDockDir):
     # if numStates is greater than 1, then split the states
     if numStates > 1:
         cmd.split_states('tmdock')
-    # delete the original tmdock
-    cmd.delete('tmdock')
+        # delete the original tmdock
+        cmd.delete('tmdock')
     # get the names of all structures in the tmdock
     names = cmd.get_names()
     # extract out any names that contain tmdock
@@ -50,19 +51,29 @@ for file in os.listdir(tmDockDir):
         rmsd = cmd.align(name, pdbName)[0]
         # add the name and rmsd to the dataframe
         rmsd_df = pd.concat([rmsd_df, pd.DataFrame([[name, rmsd]], columns=['Name', 'RMSD'])])
+    # sort the dataframe by the rmsd
+    rmsd_df.sort_values(by=['RMSD'], inplace=True)
     if rmsd_df.shape[0] > 2:
-        # sort the dataframe by the rmsd
-        rmsd_df.sort_values(by=['RMSD'], inplace=True)
         # keep the highest and lowest rmsd
-        rmsd_df = rmsd_df.iloc[[0, -1]]
+        #rmsd_df = rmsd_df.iloc[[0, -1]]
         for name in tmdock_names:
             # if the name is not in the rmsd_df, then delete it
             if name not in rmsd_df['Name'].values:
                 cmd.delete(name)
+    # output the size of the dataframe
+    # keep the lowest rmsd
+    rmsd_df = rmsd_df.iloc[[0]]
+    # replace the name with the pdbName
+    rmsd_df['Name'] = pdbName
     # save the pymol session
     cmd.save(f'{outputDir}/{pdbName}.pse')
     # reset the pymol session
     cmd.reinitialize()
+    # output the rmsd_df
+    df = pd.concat([df, rmsd_df])
+
+# save the dataframe
+df.to_csv(f'{outputDir}/rmsd.csv', index=False)
 
 
     
