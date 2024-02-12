45d5512d7c87414faae2e754c92e359082cd61c0
gilbert.work <loiseau@wisc.edu>
2024-02-12 11:42:48 -0600

    updated hbond data with pse files

---
+++ CHIP2/analyses/hbondAnalysis/2024-2-11_analysis_40percentCutoff/README.txt
@@ -0,0 +1,13 @@
+
+This contains outputs for the hbondAnalysis code. It analyzes pdb structures to determine the number of 
+hydrogen bonds that are possible based on a given distance cutoff, and it outputs analyzed data.
+
+
+hbondAnalysis
+codedir = /home/loiseau@ad.wisc.edu/github/Sequence-Design/CHIP2/analyses/hbondAnalysis/code
+psedir = /home/loiseau@ad.wisc.edu/github/Sequence-Design/CHIP2/analyses/figureMaking/2024-2-11_40percentCutoff/interfacePdbs/pse/
+outputdir = /home/loiseau@ad.wisc.edu/github/Sequence-Design/CHIP2/analyses/hbondAnalysis/2024-2-11_analysis_40percentCutoff
+hbondfile = hbondFile
+plotdatafile = plotDataFile
+datafile = /home/loiseau@ad.wisc.edu/github/Sequence-Design/CHIP2/analyses/figureMaking/2024-2-11_40percentCutoff/wt_merge_leu.csv
+requirementsfile = /home/loiseau@ad.wisc.edu/github/Sequence-Design/2022-2023_gblock/calcEnergy/requirements.txt
--- CHIP2/analyses/hbondAnalysis/code/potentialHbondCounter.py
+++ CHIP2/analyses/hbondAnalysis/code/potentialHbondCounter.py
@@ -28,7 +28,7 @@ parser.add_argument('-hbondDistance', '--hbondDistance', type=float, help='the d
 
 # extract the arguments into variables
 args = parser.parse_args()
-pseDir = args.pseDir
+inputDir = args.pseDir
 outputFile = args.outputFile
 # default values for the optional arguments
 outputDir = os.getcwd()
@@ -40,7 +40,7 @@ if args.outputDir is not None:
 if args.hbondDistance is not None:
     hbondDist = args.hbondDistance
 
-def loadPdbAndGetBonds(filename, hbondAAs, ringAAs, hbondDist=3.3):
+def loadPdbAndGetBonds(filename, hbondAAs, ringAAs, output_dir, hbondDist=3.3):
     # load the designed pdb file
     cmd.load(filename)
     # get the name of the pdb file without the extension
@@ -53,7 +53,7 @@ def loadPdbAndGetBonds(filename, hbondAAs, ringAAs, hbondDist=3.3):
     for obj in cmd.get_names():
         # select this object
         cmd.select(obj)
-        # loop through the chains of the object
+        # loop through the chains of the object to get all of the chain combinations
         chainNames = []
         for chain in cmd.get_chains(obj):
             # select the chain
@@ -67,31 +67,54 @@ def loadPdbAndGetBonds(filename, hbondAAs, ringAAs, hbondDist=3.3):
             #    # get the number of atoms on the opposite chain that are within 1.5 angstroms of the oxygen atoms
             #    numHBonds = cmd.count_atoms(f'{obj} and name O and byres (chain {chain} and resn {ringAAs[0]}+{ringAAs[1]}+{ringAAs[2]}) within 1.5')
             #    print(numHBonds)
-        donors, acceptors = 0, 0
+        # initialize the number of donors and acceptors
+        donors, acceptors, calpha_donors, calpha_acceptors = 0, 0, 0, 0
         # get all of the chainName combinations
         combinations = [[chainNames[i], chainNames[j]] for i in range(len(chainNames)) for j in range(i+1, len(chainNames))]
+        # loop through the chain combinations and get the hydrogen bond donors and acceptors
         for combo in combinations:
+            # define the donors and acceptors for each chain
+            A_donors = f'{combo[0]} and name O within {hbondDist} of {combo[1]} and h.'
+            A_acceptors = f'{combo[0]} and h. within {hbondDist} of {combo[1]} and name O'
+            B_donors = f'{combo[1]} and name O within {hbondDist} of {combo[0]} and h.'
+            B_acceptors = f'{combo[1]} and h. within {hbondDist} of {combo[0]} and name O'
+            A_calpha_donors = f'{combo[0]} and name O within 2.69 of {combo[1]} and h.'
+            A_calpha_acceptors = f'{combo[0]} and h. within 2.69 of {combo[1]} and name O'
+            B_calpha_donors = f'{combo[1]} and name O within 2.69 of {combo[0]} and h.'
+            B_calpha_acceptors = f'{combo[1]} and h. within 2.69 of {combo[0]} and name O'
+            # select the donors and acceptors and name them by the chain combination
+            cmd.select(f'donors_{combo[0]}_{combo[1]}', f'{A_donors} or {B_donors}')
+            cmd.select(f'acceptors_{combo[0]}_{combo[1]}', f'{A_acceptors} or {B_acceptors}')
+            cmd.select(f'calpha_donors_{combo[0]}_{combo[1]}', f'{A_calpha_donors} or {B_calpha_donors}')
+            cmd.select(f'calpha_acceptors_{combo[0]}_{combo[1]}', f'{A_calpha_acceptors} or {B_calpha_acceptors}')
+            # add them to the total count
+            donors = donors + cmd.count_atoms(f'donors_{combo[0]}_{combo[1]}')
+            acceptors = acceptors + cmd.count_atoms(f'acceptors_{combo[0]}_{combo[1]}')
+            calpha_donors = calpha_donors + cmd.count_atoms(f'calpha_donors_{combo[0]}_{combo[1]}')
+            calpha_acceptors = calpha_acceptors + cmd.count_atoms(f'calpha_acceptors_{combo[0]}_{combo[1]}')
             #numHbondA = cmd.count_atoms(f'{combo[0]} and name O and byres (resn {hbondAAs[0]}+{hbondAAs[1]}+{hbondAAs[2]}) within 3.3 of {combo[1]}')
             #numHbondB = cmd.count_atoms(f'{combo[1]} and name O and byres (resn {hbondAAs[0]}+{hbondAAs[1]}+{hbondAAs[2]}) within 3.3 of {combo[0]}')
-            A_donors = cmd.count_atoms(f'{combo[0]} and name O within {hbondDist} of {combo[1]} and h.')
-            A_acceptors = cmd.count_atoms(f'{combo[0]} and h. within {hbondDist} of {combo[1]} and name O')
-            B_donors = cmd.count_atoms(f'{combo[1]} and name O within {hbondDist} of {combo[0]} and h.')
-            B_acceptors = cmd.count_atoms(f'{combo[1]} and h. within {hbondDist} of {combo[0]} and name O')
-            A_calpha_donors = cmd.count_atoms(f'{combo[0]} and name O within 2.69 of {combo[1]} and h.')
-            A_calpha_acceptors = cmd.count_atoms(f'{combo[0]} and h. within 2.69 of {combo[1]} and name O')
-            B_calpha_donors = cmd.count_atoms(f'{combo[1]} and name O within 2.69 of {combo[0]} and h.')
-            B_calpha_acceptors = cmd.count_atoms(f'{combo[1]} and h. within 2.69 of {combo[0]} and name O')
-        donors += A_donors + B_donors
-        acceptors += A_acceptors + B_acceptors
-        calpha_donors = A_calpha_donors + B_calpha_donors
-        calpha_acceptors = A_calpha_acceptors + B_calpha_acceptors
+            #A_donors = cmd.count_atoms(f'{combo[0]} and name O within {hbondDist} of {combo[1]} and h.')
+            #A_acceptors = cmd.count_atoms(f'{combo[0]} and h. within {hbondDist} of {combo[1]} and name O')
+            #B_donors = cmd.count_atoms(f'{combo[1]} and name O within {hbondDist} of {combo[0]} and h.')
+            #B_acceptors = cmd.count_atoms(f'{combo[1]} and h. within {hbondDist} of {combo[0]} and name O')
+            #A_calpha_donors = cmd.count_atoms(f'{combo[0]} and name O within 2.69 of {combo[1]} and h.')
+            #A_calpha_acceptors = cmd.count_atoms(f'{combo[0]} and h. within 2.69 of {combo[1]} and name O')
+            #B_calpha_donors = cmd.count_atoms(f'{combo[1]} and name O within 2.69 of {combo[0]} and h.')
+            #B_calpha_acceptors = cmd.count_atoms(f'{combo[1]} and h. within 2.69 of {combo[0]} and name O')
+        #donors += A_donors + B_donors
+        #acceptors += A_acceptors + B_acceptors
+        #calpha_donors = A_calpha_donors + B_calpha_donors
+        #calpha_acceptors = A_calpha_acceptors + B_calpha_acceptors
         # add the data to the output dataframe using concat
         output_df = pd.concat([output_df, pd.DataFrame({'Sequence': sequence, 'object_name': obj, 'hbondAcceptors': acceptors, 'hbondDonors': donors, 'c-alphaDonors':calpha_donors, 'c-alphaAcceptors':calpha_acceptors}, index=[0])])
+    # save the session file
+    cmd.save(f'{output_dir}/{sequence}.pse')
     # close the pdb file
     cmd.reinitialize()
     return output_df
 
-def measurePotentialHBonds(raw_data_dir, hbondAAs, ringAAs):
+def measurePotentialHBonds(raw_data_dir, hbondAAs, ringAAs, output_dir):
     # initialize the output datafram
     output_df = pd.DataFrame()
     # loop through the files in the rawDataDir
@@ -100,7 +123,7 @@ def measurePotentialHBonds(raw_data_dir, hbondAAs, ringAAs):
             # get the filename
             filename = raw_data_dir+'/'+file
             # load the pdb file and get the hydrogen bonds
-            bond_df = loadPdbAndGetBonds(filename, hbondAAs, ringAAs)
+            bond_df = loadPdbAndGetBonds(filename, hbondAAs, ringAAs, output_dir)
             output_df = pd.concat([output_df, bond_df])
     return output_df
     
@@ -112,12 +135,16 @@ if __name__ == '__main__':
     
     # loop through the pseDir (which contains the directories of pse files for each design region)
     outputDf = pd.DataFrame()
-    for d in os.listdir(pseDir):
+    for d in os.listdir(inputDir):
         # define the current directory (current design region)
-        currDir = f'{pseDir}/{d}'
+        currDir = f'{inputDir}/{d}'
         if os.path.isdir(currDir):
+            print(currDir)
+            # make the output directory for the pse files
+            pseDir = f'{outputDir}/pse/{d}'
+            os.makedirs(pseDir, exist_ok=True)
             # get the potential hydrogen bonds
-            currHbonds = measurePotentialHBonds(currDir, hbondAAs, ringAAs)
+            currHbonds = measurePotentialHBonds(currDir, hbondAAs, ringAAs, pseDir)
             outputDf = pd.concat([outputDf, currHbonds])
     # save the output dataframe to a csv file
     outputDf.to_csv(f'{outputDir}/{outputFile}.csv', index=False)
